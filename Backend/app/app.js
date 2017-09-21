09566144ee87ef6b385e98d23b97adbb7f7a1557
MarcLazo <lazolatorre@et.esiea.fr>
2017-09-21 13:23:06 +0200

    creation du controller de table de jeu 'Table'

--- Backend/app/app.js
+++ Backend/app/app.js
@@ -5,8 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var api = require('./routes/apiRest');
 
 var app = express();
 
@@ -22,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api');
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ Backend/app/models/Game.js
@@ -0,0 +1,14 @@
+/**
+ * Created by MARC LAZOLA TORRE on 21/09/2017.
+ */
+
+function Game() {}
+
+Game.prototype.add = function() {
+    return "blabla";
+};
+
+module.exports = {
+    game: Game,
+}
+
