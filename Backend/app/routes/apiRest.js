99d4bb57966914e1e5e93df1db9e95cdbb51b6e3
MarcLazo <lazolatorre@et.esiea.fr>
2017-10-24 15:11:35 +0200

    Changement du package.json

--- Backend/app/routes/apiRest.js
+++ Backend/app/routes/apiRest.js
@@ -1,178 +1,81 @@
 /**
  * all api request declaration
  */
-
 var express = require('express');
+var url = require('url');
+var querystring = require('querystring');
 var router = express.Router();
-var Tables = require('../controllers/Tables').tables;
-
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost/Blackjackdb";
+var Tables = require('../controllers/Tables');
 
 var tables = new Tables();
 
 /**
  * get information of the table
  */
-router.get('/viewGames', function(req, res, next) {
-    res.send(tables.games);
-    //res.render('index', { title: 'Express' });
+router.get('/viewTables', function (req, res, next) {
+  var a = {
+    response: tables.createAGame(),
+  };
+
+  console.log(querystring.parse(url.parse(req.url).query));
+
+  res.json(a);
 });
-
-/* POST new user. */
-router.get('/login', function(req, res, next) {
-
-    var username = req.param('username');
-    var pwd = req.param('pwd');
-
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        var query = { username: username };
-        //Verify the username if it already exists.
-        db.collection("users").find(query).toArray(function(err, result) {
-            if (err) throw err;
-            if(result[0] != null){
-                res.send('Username already existed');
-                //res.send(result);
-                db.close();
-            }else{
-                var newuser = { username: username, pwd: pwd, money: "1000" };
-                db.collection("users").insertOne(newuser, function(err, res){
-                    if (err) throw err;
-                    console.log("1 document inserted");
-                    db.close();
-                });
-                //res.send('New document '.concat(username,' added'));
-                res.send(result);
-            }
-        });
-        
-    });
-    
-});
-
 
 /**
  * create an new game table
  */
-router.get('/createGame', function(req, res, next) {
-    var iduser = req.param('iduser');
-    var gamename = req.param('gamename');
-    var i;
-    var flag = new Boolean(false);
-
-    for(i=0; i < tables.games.length; i++){
-
-        if(tables.games[i].gamename == gamename){
-            console.log('This name already exist.')
-            flag = new Boolean(true);
-            return;
-        }
-    }
-    if(flag == Boolean(false)){
-        var a = tables.createAGame(iduser,gamename);
-        console.log(a);
-    }
-    
-    
-    res.render('index', { title: 'Express' });
+router.get('/createTables', function (req, res, next) {
+  res.render('index', {title: 'Express'});
 });
 
 /**
- * sign a player
+ * log a player
  */
-router.get('/signin', function(req, res, next) {
-    var username = req.param('username');
-    var pwd = req.param('pwd');
-
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        var query = { username: username, pwd: pwd };
-        db.collection("users").find(query).toArray(function(err, result) {
-            if (err) throw err;
-            console.log(result);
-            res.send(result);
-            db.close();
-        });
-    });
+router.post('/login', function (req, res, next) {
+  res.render('index', {title: 'Express'});
 });
 
 /**
  * logout a player
  */
-router.post('/logout', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+router.post('/logout', function (req, res, next) {
+  res.render('index', {title: 'Express'});
 });
 
 /**
- * UPDATE a player join the game
+ * a player join the table
  */
-router.get('/joinGame', function(req, res, next) {
-    var gamename = req.param('gamename');
-    var iduser = req.param('iduser');
-    var i;
-    var flag = new Boolean(false);
-
-    for(i=0; i < tables.games.length; i++){
-        
-        if(tables.games[i].gamename == gamename){
-
-            tables.games[i].addPlayer(iduser);
-            flag = new Boolean(true);
-            return;
-        }
-        
-    }
-    if(flag == Boolean(false)){
-        console.log('This Game not exists.')
-    }
-    res.render('index', { title: 'Express' });
+router.post('/joinTable', function (req, res, next) {
+  res.render('index', {title: 'Express'});
 });
 
 /**
- *  UPDATE a player leave the game
+ * a player leave the table
  */
-router.get('/leaveGame', function(req, res, next) {
-    var gamename = req.param('gamename');
-    var iduser = req.param('iduser');
-    var i;
-    var flag = new Boolean(false);
-
-    for(i=0; i < tables.games.length; i++){
-        
-        if(tables.games[i].gamename == gamename){
-
-            tables.games[i].removePlayer(iduser);
-            flag = new Boolean(true);
-            return;
-        }
-        
-    }
-    if(flag == Boolean(false)){
-        console.log('This Game not exists.')
-    }
-    res.render('index', { title: 'Express' });
+router.post('/leaveTable', function (req, res, next) {
+  res.render('index', {title: 'Express'});
 });
 
 /**
  * a player bet an amount of money and keep his cards
  */
-router.post('/bet', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+router.post('/bet', function (req, res, next) {
+  res.render('index', {title: 'Express'});
 });
 
 /**
  * the player want another card
  */
-router.post('/hit', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+router.post('/hit', function (req, res, next) {
+  res.render('index', {title: 'Express'});
 });
 
 /**
  * the player keep his card and don't want more
  */
-router.post('/stand', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+router.post('/stand', function (req, res, next) {
+  res.render('index', {title: 'Express'});
 });
 
 /**
@@ -181,22 +84,22 @@ router.post('/stand', function(req, res, next) {
  * the player to increase the bet by amounts other than 100%. Non-controlling players may double their wager or decline
  * to do so, but they are bound by the controlling player's decision to take only one card.
  */
-router.post('/doubleDown', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+router.post('/doubleDown', function (req, res, next) {
+  res.render('index', {title: 'Express'});
 });
 
 /**
  * the player leave his cards (only available on the first call/decision)
  */
-router.post('/surrender', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+router.post('/surrender', function (req, res, next) {
+  res.render('index', {title: 'Express'});
 });
 
 /**
  *
  */
-router.post('/split', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+router.post('/split', function (req, res, next) {
+  res.render('index', {title: 'Express'});
 });
 
 module.exports = router;
