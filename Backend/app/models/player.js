7ef42a567c42fa3fa12ef62a6653c27f7d21a5e0
MarcLazo <lazolatorre@et.esiea.fr>
2017-10-24 15:12:24 +0200

    On utilise ES6 pour la class Player

--- Backend/app/models/player.js
+++ Backend/app/models/player.js
@@ -1,10 +1,24 @@
-function Player(){
-	this.id;
-	this.username;
-	this.pwd;
-	this.hand = [];
-	this.money;
+class Player {
+  id;
+  username;
+  pwd;
+  hand = [];
+  money;
+
+  constructor(id, username, pwd, money) {
+    this.id = id;
+    this.username = username;
+    this.pwd = pwd;
+    this.money = money;
+  }
+
+  constructor(id, username, pwd) {
+    this.id = id;
+    this.username = username;
+    this.pwd = pwd;
+  }
 }
+
 
 module.exports = {
     player: Player,
