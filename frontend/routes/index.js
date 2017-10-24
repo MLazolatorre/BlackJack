3d236eb45b238c5e4b065551e003f7a3d5222069
Yoann Kidjo <yoann.kidjo@gmail.com>
2017-10-24 13:42:57 +0200

    ajout fichiers frontend

---
+++ frontend/routes/index.js
@@ -0,0 +1,17 @@
+var cardsJSON = require ('../cards.json');
+
+//home
+exports.home = function (req, res) {
+
+    var cards = cardsJSON.cards;
+
+    res.render('home.ejs', {
+        title : "BlackJack",
+        cards : cards
+    });
+};
+
+//not found
+exports.not_found = function (req, res){
+    res.send("This is not the page you are looking for");
+};
