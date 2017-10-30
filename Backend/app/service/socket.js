eddba49318e0be56b62d228008c898f510982139
MarcLazo <lazolatorre@et.esiea.fr>
2017-10-30 09:40:35 +0100

    Socket back ok

--- Backend/app/service/socket.js
+++ Backend/app/service/socket.js
@@ -10,24 +10,46 @@ let io;
 function listen (app){
   io = socketio.listen(app);
 
-  const tablesId = Tables.getIdTable();
-
   io.sockets.on('connection', function(socket){
     console.log('une connection io');
 
     let playerId = false;
 
     emitter.on('login', (player) => {
-      console.log('log %d', player);
+      console.log('player %d logged in', player);
       playerId = player;
     });
 
-    socket.on('room', function(room) {
+    socket.on('joinRoom', (room) => {
       socket.join(room);
+      io.sockets.in(room).emit('newPlayer', Players.getById(playerId));
     });
 
+    socket.on('leaveRoom', (room) => {
+      socket.leave(room);
+    });
+
+    // disconnect the player if we lose the connection
     socket.on('disconnect', () => {
       if (playerId) Players.logout(playerId)
+    });
+  });
+
+  emitter.on('allplay', (room) => {
+    io.sockets.in(room).emit('allPlay');
+  });
+
+  emitter.on('bet', (room, playerId, bet) => {
+    io.sockets.in(room).emit('bet', {
+      playerId,
+      bet,
+    });
+  });
+
+  emitter.on('hit', (room, playerId, card) => {
+    io.sockets.in(room).emit('hit', {
+      playerId,
+      card,
     });
   });
 
